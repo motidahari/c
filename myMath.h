aae1bf7b9eae5d03cbf860326794fb6054076cae
MotiDahari <motidaharii@gmail.com>
2020-11-11 17:14:29 +0200

    upload files to github
    upload files to github

---
+++ myMath.h
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+/**
+ * created by yochai modechai dahari
+ * ID: 308212570
+*/
+
+
+/**
+ * name function: add
+ * @param: x typy double, y type float
+ * @return: result(x+y)
+*/
+float add(float x , float y);
+/**
+ * name function: sub
+ * @param: x typy float, y type float
+ * @return: result(x-y)
+*/
+float sub(float x , float y);
+/**
+ * name function: mul
+ * @param: x typy double, y type int
+ * @return: result(x*y)
+*/
+double mul(double x , int y);
+/**
+ * name function: div
+ * @param: x typy double, y type int
+ * @return: result(x/y)
+*/
+double div(double x, int y);
+/**
+ * name function: Exp
+ * @param: x typy int
+ * @return: result
+*/
+double Exp(int x);
+/**
+ * name function: Pow
+ * @param: x typy double, y type int
+ * @return: result
+*/
+double Pow(double x , int y);
+
+
+
+
+
+
+
+
