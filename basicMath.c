daaa8d43b1de1465f732b6794f1980d396789ff3
MotiDahari <motidaharii@gmail.com>
2020-11-13 14:28:09 +0200

    finish the projectx ex1

---
+++ basicMath.c
@@ -0,0 +1,41 @@
+#include <stdio.h>  
+#include "myMath.h"
+/**
+ * created by yochai modechai dahari
+ * ID: 308212570
+*/
+
+
+/**
+ * name function: add
+ * @param: x typy double, y type float
+ * @return: result(x+y)
+*/
+float add(float x , float y){
+    return x+y;
+}
+/**
+ * name function: sub
+ * @param: x typy float, y type float
+ * @return: result(x-y)
+*/
+float sub(float x , float y){
+    return x-y;
+}
+/**
+ * name function: mul
+ * @param: x typy double, y type int
+ * @return: result(x*y)
+*/
+double mul(double x , int y){
+    return x*y;
+}
+/**
+ * name function: div
+ * @param: x typy double, y type int
+ * @return: result(x/y)
+*/
+
+double div(double x, int y){
+    return x/y;
+}
